3a82e8802f5b03e7197052ec59122615d4370fb0
anchit-chandran <anchit97123@gmail.com>
2024-01-18 09:39:02 +0000

    Fix count_threes function and function signature
    Update script.m with correct function syntax
    
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- count_threes_exercise.m
+++ count_threes_exercise.m
@@ -1,4 +1,5 @@
 count_threes::[num]->num
 count_threes [] = 0
 count_threes (x : rest) = 1 + (count_threes rest), if x = 3
-		 	= 0 + (count_threes rest), otherwise
+		 	= 0 + (count_threes rest), if x != num
+			= 0 + (count_threes rest), otherwise
--- script.m
+++ script.m
@@ -1,8 +1,8 @@
 str == [char]
 vector == (num, num, num)
 
-fn::(str, vector) -> (str, vector)
-fn msg v1 = (msg, v1)
+fn :: (str, vector) -> (str, vector)
+fn (msg, v1) = (msg, v1)
 
 
 
