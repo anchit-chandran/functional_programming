5c220f7c7e6fef20e5c15390f09cd9289b063cf9
anchit-chandran <anchit97123@gmail.com>
2024-01-18 12:59:38 +0000

    Refactor count_threes function to handle variable type checking
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- count_threes_exercise.m
+++ count_threes_exercise.m
@@ -1,5 +1,5 @@
 count_threes::[num]->num
 count_threes [] = 0
-count_threes (x : rest) = 1 + (count_threes rest), if x = 3
-		 	= 0 + (count_threes rest), if x != num
+count_threes (x : rest) = 1 + (count_threes rest), if x = 3 
+			|| = 0 + (count_threes rest), if x != num HOW TO TYPE CHECK A VAR?
 			= 0 + (count_threes rest), otherwise
