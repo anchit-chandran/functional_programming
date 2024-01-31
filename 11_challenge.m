684678900f6e43e1d513100f37c7289403dc9713
anchit-chandran <anchit97123@gmail.com>
2024-01-31 14:12:24 +0000

    initial working code
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- 11_challenge.m
+++ 11_challenge.m
@@ -2,11 +2,25 @@
 || 1. number of 1s before first 0
 || 2. number of 2s before first 0
 || 3. length of longest sequence of 1s before first 0
-|| e.g. count_pre_zero ([1,1,2,2,1,2,1,0,1,2,0]) = (4, 3, 1)
+|| e.g. count_pre_zero ([1,1,2,2,1,1,1,2,1,0,1,2,0]) = (6, 3, 3)
 
-|| Takes in a list of nums, returns count of 1s before first 0. E.g. count_1s([1,1,2,2,1,2,1,0,1,2,0]) = 4
-count_1s :: [num] -> num
-count_1s (1:xs) = 1 + count_1s(xs)
-count_1s (0 : xs) = 0 || found a 0, stop iteration
-count_1s (x:xs) = count_1s(xs)
 
+count_pre_zero :: [num] -> (num,num,num)
+count_pre_zero xs = (count_nums_before_0(1, xs),count_nums_before_0(2, xs), longest_subseq_before_0(xs))
+
+count_nums_before_0 :: (num, [num]) -> num
+count_nums_before_0 (n, 0 : xs) = 0 || base case is zero as guaranteed to find this before empty list
+count_nums_before_0 (n, n : xs) = 1 + count_nums_before_0(n, xs)
+count_nums_before_0 (n, x : xs) = count_nums_before_0(n, xs)
+
+|| longest subsequence of 1s before first 0
+longest_subseq_before_0 :: [num] -> num
+longest_subseq_before_0 xs = x_long_subseq_before_0(xs, 0, 0)
+                             where
+                             x_long_subseq_before_0(x : xs, curr_longest, max_longest) = larger_num(curr_longest, max_longest), if x=0
+                                                                                       = x_long_subseq_before_0(xs, curr_longest+1, max_longest), if x=1
+                                                                                       = x_long_subseq_before_0(xs, 0, larger_num(curr_longest, max_longest)), otherwise
+
+larger_num :: (num,num) -> num
+larger_num (a,b) = a, if a > b
+               = b, otherwise
