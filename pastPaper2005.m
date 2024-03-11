c84ea6ecf1afc28ee813e37cc2ffe05977a2aeaf
anchit-chandran <anchit97123@gmail.com>
2024-03-11 10:56:23 +0000

    adds comments to shuffle
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- pastPaper2005.m
+++ pastPaper2005.m
@@ -2,6 +2,8 @@
 suit ::= Hearts | Spades | Clubs | Diamonds
 card == (num, suit)
 
+|| Q2b ---
+
 || e.g. if deck had 4 els, n will start as 2. If 5, n also cast to 2.
 first_half :: num -> [card] -> [card]
 first_half 0 rest = []
@@ -14,18 +16,17 @@ second_half 0 any drop_last
 second_half n (front : rest) drop_last = second_half (n-1) rest drop_last
 
 || This function takes in [card] and returns it split in half ([cards], [cards]). If odd number, discard last.
-|| split_half test_deck_1
-|| split_half test_deck_2
 split_half :: [card] -> ( [card], [card] )
 split_half(deck) 
     = (first_half half_index deck, second_half half_index deck drop_last)
         where 
         deck_len = #deck
-        half_index = entier ((deck_len)/2)
+        half_index = entier ((deck_len)/2) || cast float to int
         drop_last 
             = False, if (deck_len mod 2) = 0 || drop last element in odd lists
             = True, otherwise
 
+|| interleave takes tuple of equal sized [card] and interleaves starting with first card of second deck
 interleave :: ([card],[card]) -> [card]
 interleave ( first_half, second_half ) 
     = xinterleave ( first_half, second_half ) True || start with second half, interleave
@@ -35,16 +36,6 @@ interleave ( first_half, second_half )
         xinterleave ( (f_first : f_rest), (s_first : s_rest) ) get_second
             = s_first : xinterleave ( (f_first : f_rest), (s_rest) ) False, if get_second
             = f_first : xinterleave ( (f_rest), (s_first : s_rest) ) True, otherwise
-
-|| interleave ( [card1, card2], [card3, card4] )
-|| xinterleave ( [card1, card2], [card3, card4] ) True 
-|| card3 : xinterleave ( [card1, card2], [card4] ) False
-|| card3 : (card1 : xinterleave ( [card2], [card4] ) True)
-|| card3 : (card1 : (card4 : xinterleave ( [card2], [] ) False))
-|| card3 : (card1 : (card4 : ([card2])))
-|| card3 : (card1 : ([card4, card2]))
-|| card3 : ([card1, card4, card2])
-|| [card3, card1, card4, card2]
 
 shuffle :: num -> [card] -> [card]
 shuffle num_shuffles deck
